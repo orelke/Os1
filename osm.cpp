2c84486c404c5a6f58ed9638b563254e414916e6
orelke <orel.keliahu@mail.huji.ac.il>
2018-04-16 22:23:23 +0300

    zain

--- osm.cpp
+++ osm.cpp
@@ -41,6 +41,11 @@ int osm_finalizer()
 
 double osm_operation_time(unsigned int iterations)
 {
+    unsigned int totalIterations = iterations == INVALID ? ITERATION_DEFAULT : iterations;
+    int x1 = 0, x2 = 0, x3 = 0, x4 = 0, x5 = 0, y = 3 , z = 4;
+    auto rounds = (unsigned int)std::ceil(totalIterations / UNROLLING_FACTOR);
+
+
 
     if(osm_init() == ERROR)
     {
@@ -48,24 +53,15 @@ double osm_operation_time(unsigned int iterations)
     }
 
 
-    unsigned int totalIterations = iterations == INVALID ? ITERATION_DEFAULT : iterations;
-    int x1 = 0, x2 = 0, x3 = 0, x4 = 0, x5 = 0, y = 3 , z = 4;
-    auto rounds = (unsigned int)std::ceil(totalIterations / UNROLLING_FACTOR);
-
     for(unsigned int i = 0; i < rounds ; i++)
     {
-        x1 = z + y;
-        x2 = z + y;
-        x3 = z + y;
-        x4 = z + y;
-        x5 = z + y;
+        x1 = y + z;
+        x2 = y + z;
+        x3 = y + z;
+        x4 = y + z;
+        x5 = y + z;
     }
 
-    (void)x1;
-    (void)x2;
-    (void)x3;
-    (void)x4;
-    (void)x5;
 
     if(osm_finalizer() == ERROR)
     {
@@ -75,18 +71,27 @@ double osm_operation_time(unsigned int iterations)
     double time = ((end.tv_sec - start.tv_sec) * SEC_TO_NANO) +
                   (end.tv_usec - start.tv_usec) * MICRO_TO_NANO;
 
+
+
+    (void)x1;
+    (void)x2;
+    (void)x3;
+    (void)x4;
+    (void)x5;
+
     return time / totalIterations;
 }
 
 double osm_function_time(unsigned int iterations)
 {
 
+    unsigned int totalIterations = iterations == 0 ? ITERATION_DEFAULT : iterations;
+    auto rounds = (unsigned int)std::ceil(totalIterations / UNROLLING_FACTOR);
+
     if(osm_init() == ERROR)
     {
         return ERROR;
     }
-    unsigned int totalIterations = iterations == 0 ? ITERATION_DEFAULT : iterations;
-    auto rounds = (unsigned int)std::ceil(totalIterations / UNROLLING_FACTOR);
 
     for(unsigned int i = 0; i < rounds; i++)
     {
@@ -101,6 +106,7 @@ double osm_function_time(unsigned int iterations)
     {
         return ERROR;
     }
+
     double time = ((end.tv_sec - start.tv_sec) * SEC_TO_NANO) +
                   (end.tv_usec - start.tv_usec) * MICRO_TO_NANO;
 
@@ -109,13 +115,15 @@ double osm_function_time(unsigned int iterations)
 
 double osm_syscall_time(unsigned int iterations)
 {
+
+    unsigned int totalIterations = iterations == 0 ? ITERATION_DEFAULT : iterations;
+    auto rounds = (unsigned int)std::ceil(totalIterations / UNROLLING_FACTOR);
+
+
     if(osm_init() == ERROR)
     {
         return ERROR;
     }
-
-    unsigned int totalIterations = iterations == 0 ? ITERATION_DEFAULT : iterations;
-    auto rounds = (unsigned int)std::ceil(totalIterations / UNROLLING_FACTOR);
 
     for(unsigned int i = 0; i < rounds; i ++)
     {
